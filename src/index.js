4208d7afc382b684f4069f48e8308623378e8244
Uglekiska <uglekiska@yandex.by>
2021-08-15 15:32:20 +0300

    fix: end index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (typeof array === "undefined" || array === null || array === '' || isNaN(array[1]) || typeof array === 'Infinity') {
+        return 0;
+    } else {
+    return Math.min.apply(null, array);
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof array === "undefined" || array === null || array === '' || isNaN(array[1]) || typeof array === 'Infinity') {
+        return 0;
+    } else {   
+    return Math.max.apply(null, array);
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array === "undefined" || array === null || array === '' || isNaN(array[1]) || typeof array === 'Infinity') {
+        return 0;
+    }    
+    let sum = 0; 
+    for(var i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
 }
